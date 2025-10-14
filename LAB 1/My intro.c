c56e09f7cd7c9a8d1a317036a60d43499e1dcf20
Saumya Saurabh <meb25057@tezu.ac.in>
2025-10-14 21:01:03 +0530

    Add files via upload
    1. Introduction to C .
    2. My introduction :name,roll,department,university.

---
+++ LAB 1/My intro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	printf("NAME:SAUMYA SAURABH\n");
+	printf("ROLL NO.:MEB25057\n");
+	printf("DEPARTMENT:MECHANICAL ENGINEERING\n");
+	printf("UNIVERSITY:TEZPUR UNIVERSITY");
+	
+	
+	
+	
+	return 0;
+}
